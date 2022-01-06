bad7ff2bdafd532aafb89b63aa0a953cd37f78d6
MarcusReitzl <mareitzl@edu.aau.at>
2022-01-06 23:30:31 +0100

    header and navbar

---
+++ script.js
@@ -0,0 +1,17 @@
+const container = document.querySelector(".container");
+
+document.querySelector(".open-navbar-icon").addEventListener("click", () => {
+  container.classList.add("change");
+});
+
+document.querySelector(".close-navbar-icon").addEventListener("click", () => {
+  container.classList.remove("change");
+});
+
+const colors = ["#660000", "#990000", "#A0A0A0", "#606060", "#202020"];
+
+let i = 0;
+
+Array.from(document.querySelectorAll(".nav-link")).forEach(item => {
+  item.style.cssText = `background-color: ${colors[i++]}`;
+});
